22483d26bc5b41e75df30a274a60ba969002aa1e
Romis Sousa <estreladamanha585@gmail.com>
2020-07-22 10:10:01 -0300

    tirar  for em excesso

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,12 +2,9 @@ defmodule Cards do
   def create_deck  do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
-    cards = for value <- values do
-      for suit <- suits do
+    for suit <- suits, value <- values do
         "#{value} of #{suit}"
-      end
     end
-    List.flatten(cards)
   end
   def shuffle(deck) do
      Enum.shuffle(deck)

40218e50407c8b5134fd9ca42a9d2a5614801d7b
Romis Sousa <estreladamanha585@gmail.com>
2020-07-22 10:04:15 -0300

    nested list

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,18 @@
 defmodule Cards do
   def create_deck  do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    cards = for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
+    end
+    List.flatten(cards)
   end
   def shuffle(deck) do
      Enum.shuffle(deck)
+  end
+  def contains?(deck, card) do
+     Enum.member?(deck, card)
   end
 end

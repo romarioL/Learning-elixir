1e055fdbb1330b74680dd300152a3d57c1cf10fb
Romis Sousa <estreladamanha585@gmail.com>
2020-07-22 10:35:33 -0300

    add spaces

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,5 @@
 defmodule Cards do
+
   def create_deck  do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -6,10 +7,16 @@ defmodule Cards do
         "#{value} of #{suit}"
     end
   end
+
   def shuffle(deck) do
      Enum.shuffle(deck)
   end
+
   def contains?(deck, card) do
      Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+     Enum.split(deck, hand_size)
   end
 end
